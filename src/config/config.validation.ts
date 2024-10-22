08e1c9c7ac384bac15d9ff9a65975600c4ac6135
YourAKShaw <hitme@yourakshaw.com>
2024-10-22 15:30:59 +0530

    chore(env): :closed_lock_with_key: add joi based config validation

---
+++ src/config/config.validation.ts
@@ -0,0 +1,10 @@
+import * as Joi from 'joi';
+
+export const configValidationSchema = Joi.object({
+  PORT: Joi.number().default(3000),
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+  MONGODB_URI: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+});
