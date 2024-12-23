c74b9cd00c23c9dbd8af4356dd7624d1cb34f03c
YourAKShaw <hitme@yourakshaw.com>
2024-12-23 14:32:02 +0530

    fix(user): :bug: fix user schema

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -37,13 +37,3 @@ export class User {
 }
 
 export const UsersSchema = SchemaFactory.createForClass(User);
-
-// Create case-insensitive indexes
-UsersSchema.index(
-  { username: 1 },
-  { unique: true, collation: { locale: 'en', strength: 2 } },
-);
-UsersSchema.index(
-  { email: 1 },
-  { unique: true, collation: { locale: 'en', strength: 2 } },
-);
