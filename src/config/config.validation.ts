67bf8a6d36e7995c691bc31870f2ff1df795db62
YourAKShaw <hitme@yourakshaw.com>
2024-12-23 14:42:56 +0530

    chore(env): :closed_lock_with_key: update config.validation.ts

--- src/config/config.validation.ts
+++ src/config/config.validation.ts
@@ -7,4 +7,7 @@ export const configValidationSchema = Joi.object({
     .default('development'),
   MONGO_URI: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
+  TWILIO_ACCOUNT_SID: Joi.string().required(), // Validate Twilio Account SID
+  TWILIO_AUTH_TOKEN: Joi.string().required(), // Validate Twilio Auth Token
+  TWILIO_VERIFY_SERVICE_SID: Joi.string().required(), // Validate Twilio Verify Service SID
 });
